aa7d51aecbea1ba321d7970a8b008db3f432ca3a
JustinNC18 <JustinNC18@gmail.com>
2026-09-02 16:38:39 -0500

    Update 01_pull_c17002.py

--- python/01_pull_c17002.py
+++ python/01_pull_c17002.py
@@ -1,17 +1,35 @@
-## C17002 2024 data. JSON to CSV.ipynb
+"""
+01_pull_c17002.py
+
+Purpose:
+Retrieve ACS 2024 C17002 poverty data for all ZIP Code
+Tabulation Areas (ZCTAs) and save results as a CSV file.
+
+Project:
+Harris County SNAP Participation Opportunity Analysis
+
+Source:
+U.S. Census Bureau ACS 5-Year Estimates
+"""
 
 import requests
 import pandas as pd
 
-url = "https://api.census.gov/data/2024/acs/acs5?get=NAME,C17002_001E,C17002_002E,C17002_003E,C17002_004E,C17002_005E,C17002_006E,C17002_007E&for=zip%20code%20tabulation%20area:*&key=9581abccba273ad7455fdfdeb2389836241b4f1c"
+# Census API key removed from source code
+API_KEY = "YOUR_CENSUS_API_KEY"
 
+# ACS C17002 variables
 
+url = "https://api.census.gov/data/2024/acs/acs5?get=NAME,C17002_001E,C17002_002E,C17002_003E,C17002_004E,C17002_005E,C17002_006E,C17002_007E&for=zip%20code%20tabulation%20area:*&key={API_KEY}"
+
+# Request data from Census API
 data = requests.get(url).json()
 
 df = pd.DataFrame(data[1:], columns=data[0])
 
+#Save results to csv
 df.to_csv("acs_snap_eligibility.csv", index=False)
 
+#Validation output
 print(df.head())
 
-## Saved results to csv
