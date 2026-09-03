9a2da7d0f520492f8735970549f5f5f3aa796bdf
JustinNC18 <JustinNC18@gmail.com>
2026-09-03 14:13:11 -0500

    Update 05_snap_gap_table_county_export.sql

--- sql/05_snap_gap_table_county_export.sql
+++ sql/05_snap_gap_table_county_export.sql
@@ -3,3 +3,32 @@
 05_snap_gap_table_county_export.sql
 
 Purpose:
+Export filtered table to csv based on gulf coast counties of future interest. For current project, we will just focus on Harris County.
+
+Project:
+Harris County Participation Oppurtunity Analysis
+
+Source:
+snap-gap-analysis dataset in BigQuery.
+
+**
+
+SELECT *
+FROM `snap-gap-analysis.snap_gap.snap_gap_analysis_2024_3`
+WHERE county_name IN (
+  'Austin County, Texas' ,
+  'Brazoria County, Texas' ,
+  'Chambers County, Texas' ,
+  'Colorado County, Texas' ,
+  'Fort Bend County, Texas' ,
+  'Galveston County, Texas' ,
+  'Harris County, Texas' ,
+  'Liberty County, Texas' ,
+  'Matagorda County, Texas' ,
+  'Montgomery County, Texas' ,
+  'Walker County, Texas' ,
+  'Waller County, Texas' ,
+  'Wharton County, Texas');
+
+Output: SNAP Participation rates csv file for selected gulf coast counties in Texas.
+
