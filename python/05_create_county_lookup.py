8389d07305e204db3bbd6913848716d3b51bd6c3
JustinNC18 <JustinNC18@gmail.com>
2026-09-03 12:48:19 -0500

    Create 05_create_county_lookup.py

---
+++ python/05_create_county_lookup.py
@@ -0,0 +1,44 @@
+"""
+05_create_county_lookup.py
+
+Purpose:
+Create a county lookup table using ACS county data. This lookup is used to supoort county level rollups for SNAP participation analysis.
+
+Project:
+Harris County SNAP Participation Analysis
+
+Output:
+county_lookup.csv
+"""
+
+import requests
+import pandas as pd
+
+API_KEY = "YOUR_API_KEY"
+
+url = (
+    f"https://api.census.gov/data/2024/acs/acs5"
+    f"?get=NAME&for=county:*&key={API_KEY}"
+)
+
+response = requests.get(url)
+response.raise_for_status()
+
+data = response.json()
+
+# Convert JSON to DataFrame
+df = pd.DataFrame(data[1:], columns=data[0])
+
+# Create full county FIPS code
+df["county_fips"] = df["state"] + df["county"]
+
+# Rename columns for clarity
+df = df.rename(columns={
+    "NAME": "county_name"
+})
+
+# Save to CSV
+df.to_csv("county_lookup.csv", index=False)
+
+print(df.head())
+print(f"\nRows: {len(df)}")
